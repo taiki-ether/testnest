c56562f7ffe7f0b54a6bf8b2cc429b75b3bdf949
taiki-ether <taiki@directorz.co.jp>
2025-11-21 17:30:16 +0900

    コメント追加

--- setup-api.sh
+++ setup-api.sh
@@ -1,15 +1,21 @@
-# deploy
+#!/bin/sh
+
+## npmパッケージのインストール 基本的に nodeのアプリケーションはこれだけで準備が整う。(すごい) 
 npm i
 
+## systemdを使ったサービスとして管理。再起動・起動状況の確認が容易になる。
 cp  ./nestapi-ether.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl enable nestapi-ether --now
 
+## アクセス制御などを柔軟に行うため、Nginxを導入
 apt install nginx -y
 
+## Nginx用の設定ファイルを適用 (後で一緒に見ましょう) 
 cp -ip /opt/testnest/nestapi-ether.com /etc/nginx/sites-available/
 ln -nfs /etc/nginx/sites-available/nestapi-ether.com /etc/nginx/sites-enabled/
 rm /etc/nginx/sites-enabled/default
-nginx -t
 
+## 設定チェックをして起動
+#nginx -t
 systemctl restart nginx
--- setup-npm.sh
+++ setup-npm.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+## apt を使うときはまずはパッケージリストのアップデートから
 apt update
+
+## nを入れたいので nodejs npm をパッケージから入れます。
 apt install -y nodejs npm
+
+## n をnpmからインストール
 npm install n -g
+
+## 最新安定版の nodejs, npmをインストール
 n stable
+
+## aptで入れたパッケージは使わないのでアンインストール
 apt purge -y nodejs npm
 apt autoremove -y
