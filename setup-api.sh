25845459e6457b65c52d0858223ac38449bad2e1
taiki-ether <taiki@directorz.co.jp>
2025-11-21 17:09:58 +0900

    taypoの修正

--- setup-api.sh
+++ setup-api.sh
@@ -7,7 +7,7 @@ systemctl enable nest-api-ether --now
 
 apt install nginx -y
 
-cp -ip /opt/testnest/nextapi-ether.com /etc/nginx/sites-available/
+cp -ip /opt/testnest/nestapi-ether.com /etc/nginx/sites-available/
 ln -nfs /etc/nginx/sites-available/nestapi-ether.com /etc/nginx/sites-enabled/
 rm /etc/nginx/sites-enabled/default
 nginx -t
